4618e3f9837592ba10a04a1473837cf87d4215c4
agustin <agusdelva.16@gmail.com>
2020-09-22 15:24:48 -0300

    first commit

---
+++ docs/script.js
@@ -0,0 +1,30 @@
+window.sr = ScrollReveal();
+sr.reveal('.navbar', {
+    duration: 2000,
+    origin: 'bottom'
+});
+sr.reveal('.header-content-left', {
+    duration: 2000,
+    origin: 'top',
+    distance: '300px'
+});
+sr.reveal('.header-content-right', {
+    duration: 2000,
+    origin: 'right',
+    distance: '300px'
+});
+sr.reveal('.header-btn', {
+    duration: 2000,
+    origin: 'bottom',
+    delay: 1000
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+}); 
